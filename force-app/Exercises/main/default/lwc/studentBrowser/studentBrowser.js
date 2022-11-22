b53d13495491320d04e8d9df5d4ef2220cb9d5d7
Rashmi gupta <rashmivictorious@gmail.com>
2022-11-22 10:31:18 +0530

    2-5 completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+    studentList=[];
+    constructor() {
+        // Call the super constructor here...
+        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+        this.studentList = studentNames.map( (studentName, index) => {
+          return {
+            'sobjectType': 'Contact',
+            'Name': studentName,
+            'PhotoUrl': '/services/images/photo/0032D00000ZMsjzQAD',
+            'Id': index
+          };
+        });
+      }
+}
--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,12 +1,15 @@
 import { LightningElement,api } from 'lwc';
 
 export default class StudentTile extends LightningElement {
-    student ={Name:'Rashmi gupta',
+ @api   student ={Name:'Rashmi gupta',
               PhotoUrl:'/services/images/photo/0032D00000ZMsjzQAD'}; 
               @api isSelected=false;
         @api
         get tileSelected(){
         return this.isSelected?'tile selected' :'tile';
+    }
+    onStudentClick(event){
+        alert(this.student.Name);
     }
 
 
